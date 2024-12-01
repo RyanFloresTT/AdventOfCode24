f17d5adc4fba7b5c74a700e647056abdb00c5e2f
Ryan Flores <53247675+RyanFloresTT@users.noreply.github.com>
2024-12-01 08:50:19 -0800

    Add Tests for Day 1

---
+++ AdventOfCode24.Tests/UnitTests.cs
@@ -0,0 +1,20 @@
+﻿using AdventOfCode24.Day1;
+
+namespace AdventOfCode24.Tests;
+
+public class Tests
+{
+    [Test]
+    public async Task Day1_Part1()
+    {
+        int result = await DayOne.PartOneAsync("example.txt");
+        Assert.That(result, Is.EqualTo(11));
+    }
+    
+    [Test]
+    public async Task Day1_Part2()
+    {
+        int result = await DayOne.PartTwoAsync("example.txt");
+        Assert.That(result, Is.EqualTo(31));
+    }
+}
